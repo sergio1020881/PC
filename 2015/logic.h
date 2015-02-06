35c52c4e228a423c43cb2e938732634aa5f75587
sergio <sergio1020881@users.noreply.github.com>
2015-02-06 21:14:32 +0000

    Create logic.h
    06022015

---
+++ 2015/logic.h
@@ -0,0 +1,53 @@
+/************************************************************************
+Title:    LOGIC
+Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
+File:     $Id: logic.h,v 0.1 2013/12/30 15:00:00 sergio Exp $
+Software: GCC
+Hardware: 
+License:  GNU General Public License 
+Usage:    
+LICENSE:
+    Copyright (C) 2014
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+COMMENT:
+	Very Stable
+************************************************************************/
+#ifndef _LOGIC_H_
+	#define _LOGIC_H_
+/*
+** constant and macro
+*/
+/*
+** variable
+*/
+struct LOGIC{
+	//Local Variables
+	int *mem;
+	int sizeeeprom;
+	int sizeblock;
+	int page;
+	int recall;
+	int output;
+	//Local Function pointers
+	int (*read)(struct LOGIC *r, int input);
+	int (*learn)(struct LOGIC *r, int input, int output);
+	int (*quant)(struct LOGIC *r);
+	int (*remove)(struct LOGIC *r, int input);
+	int (*deleteall)(struct LOGIC *r);
+	int (*get)(struct LOGIC *r);
+	int (*diff)(int xi, int xf);
+};
+typedef struct LOGIC LOGIC;
+/*
+** procedure and function header
+*/
+LOGIC LOGICenable(int *eeprom, int sizeeeprom, int prog);
+#endif
+/***EOF***/
