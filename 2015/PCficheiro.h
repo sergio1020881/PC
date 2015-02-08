03d9db797bf815f88e08d360418c3b6400701639
sergio <sergio1020881@users.noreply.github.com>
2015-02-08 14:57:38 +0000

    Create PCficheiro.h
    08022015

---
+++ 2015/PCficheiro.h
@@ -0,0 +1,45 @@
+/************************************************************************
+Title:    PCFICHEIRO
+Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
+File:     $Id: PCficheiro.h,v 0.1 2015/02/06 14:00:00 sergio Exp $
+Software: GCC
+Hardware: 
+License:  GNU General Public License 
+Usage:    
+LICENSE:
+    Copyright (C) 2015
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+COMMENT:
+	Very Stable
+************************************************************************/
+#ifndef _PCFICHEIRO_H_
+	#define _PCFICHEIRO_H_
+/*
+** constant and macro
+*/
+/*
+** variable
+*/
+struct ficheiro{
+	//Local Variables
+	FILE *fp;
+	char filename[128];
+	char permision[8];
+	// Function Pointers
+	int (*close)(struct ficheiro *f);
+	FILE* (*open)(struct ficheiro *f);
+};
+typedef struct ficheiro FICHEIRO;
+/*
+** procedure and function header
+*/
+FICHEIRO FICHEIROenable(char *filename);
+#endif
+/***EOF***/
