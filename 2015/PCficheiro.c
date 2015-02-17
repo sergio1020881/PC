748268e3e7a6619a9eafce429037a069149386ca
sergio <sergio1020881@users.noreply.github.com>
2015-02-17 14:48:49 +0000

    17022015

--- 2015/PCficheiro.c
+++ 2015/PCficheiro.c
@@ -27,7 +27,7 @@ COMMENT:
 // fopen perror fread fwrite feof fseek ferror fclose rewind scanf sscanf getchar scanf fscanf
 #include <stdio.h>
 // calloc free realloc malloc
-//#include <stdlib.h>
+#include <stdlib.h>
 // strcpy strcmp strcat memcmp
 #include <string.h>
 // termios tcflush
@@ -63,14 +63,18 @@ FICHEIRO FICHEIROenable(char *filename)
 	/***local variables***/
 	/***Declare Functions***/
 	int FICHEIROclose(struct ficheiro *f);
+	int FICHEIROputc(struct ficheiro *f, int c);
+	int FICHEIROputs(struct ficheiro *f, const char* s);
 	FILE* FICHEIROopen(struct ficheiro *f);
 	/******/
 	FICHEIRO f;
 	//Inicialize varibles
 	strcpy(f.filename,filename);
-	strcpy(f.permision,"rw");//setting as default
+	strcpy(f.permision,"a+");//setting as default
 	//Functions pointers or Vtable to declared functions
 	f.open=FICHEIROopen;
+	f.colocarchar=FICHEIROputc;
+	f.colocarstring=FICHEIROputs;
 	f.close=FICHEIROclose;
 	//procedures
 	f.fp=fopen(f.filename,f.permision);
@@ -88,6 +92,20 @@ int FICHEIROclose(struct ficheiro *f)
 {
 	fclose(f->fp);
 	return 0;
+}
+/*colocarchar*/
+int FICHEIROputc(struct ficheiro *f, int c)
+{
+	int r;
+	r=fputc(c,(FILE*)f->fp);
+	return r;
+}
+/*colocarstring*/
+int FICHEIROputs(struct ficheiro *f, const char* s)
+{
+	int r;
+	r=fputs(s, f->fp);
+	return r;
 }
 /***open***/
 FILE* FICHEIROopen(struct ficheiro *f)
