42acbd4912fee16e908679c780edb66424eecc8c
sergio <sergio1020881@users.noreply.github.com>
2015-07-03 14:28:00 +0100

    Create PClfsm.h

---
+++ LFSM2015/PClfsm.h
@@ -0,0 +1,53 @@
+/************************************************************************
+Title:    PCLFSM
+Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
+File:     $Id: PClfsm.h,v 0.1 2015/07/03 14:00:00 sergio Exp $
+Software: GCC
+Hardware: 
+License:  GNU General Public License 
+Usage:    
+LICENSE:
+    Copyright (C) 2015
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+COMMENT:
+	review
+************************************************************************/
+#ifndef _PCLFSM_H_
+	#define _PCLFSM_H_
+/*
+** constant and macro
+*/
+/*
+** variable
+*/
+struct lfsm{
+	//Local Variables
+	int *mem;
+	int sizeeeprom;
+	int sizeblock;
+	int page;
+	int input;
+	int output;
+	//Function Pointers
+	int (*read)(struct lfsm *r, int input);
+	int (*learn)(struct lfsm *r, int input, int next);
+	int (*quant)(struct lfsm *r);
+	int (*remove)(struct lfsm *r, int input);
+	int (*deleteall)(struct lfsm *r);
+	int (*get)(struct lfsm *r);
+	int (*diff)(int xi, int xf);
+};
+typedef struct lfsm LFSM;
+/*
+** procedure and function header
+*/
+LFSM LFSMenable(int *eeprom, int sizeeeprom, int prog);
+#endif
+/***EOF***/
