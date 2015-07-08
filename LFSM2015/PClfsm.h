6d6d1a745cfe1d93d035e13bcdbec2df680ab639
sergio <sergio1020881@users.noreply.github.com>
2015-07-08 09:31:48 +0100

    Update PClfsm.h

--- LFSM2015/PClfsm.h
+++ LFSM2015/PClfsm.h
@@ -9,9 +9,8 @@ Usage:
 LICENSE:
     Copyright (C) 2015
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    any later version.
+    it under the consent of the code developer, in case of commercial use
+    need licence.
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
