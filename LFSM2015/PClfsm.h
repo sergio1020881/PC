c9527a4c8a5a9e6f6ffddd184291985b921a3b9f
sergio <sergio1020881@users.noreply.github.com>
2015-07-09 10:45:56 +0100

    Update PClfsm.h

--- LFSM2015/PClfsm.h
+++ LFSM2015/PClfsm.h
@@ -1,7 +1,7 @@
 /************************************************************************
 Title:    PCLFSM
 Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
-File:     $Id: PClfsm.h,v 0.1 2015/07/03 14:00:00 sergio Exp $
+File:     $Id: PClfsm.h,v 0.1 2015/07/09 14:00:00 sergio Exp $
 Software: GCC
 Hardware: 
 License:  GNU General Public License 
@@ -40,6 +40,7 @@ struct lfsm{
 	int (*remove)(struct lfsm *r, int input);
 	int (*deleteall)(struct lfsm *r);
 	int (*get)(struct lfsm *r);
+	void (*set)(struct lfsm *r,int output);
 	int (*diff)(int xi, int xf);
 };
 typedef struct lfsm LFSM;
@@ -49,3 +50,15 @@ typedef struct lfsm LFSM;
 LFSM LFSMenable(int *eeprom, int sizeeeprom, int prog);
 #endif
 /***EOF***/
+/***
+Sintax Semantic Pragmatic.
+Sintax is a key principle able to follow a sintactical work flow that solves any problem,
+having a concise structural work flow is very importante.
+The method used is C object oriented aproach, do not know anybody using this type of aproach, 
+but all the programs made here follow this type of paradigm, it is very very stable either in 
+PC or Embeded MCU programs, making Main program very compact.
+Discovered this method by experience since then never left it, altering prior programs to fit 
+its syntax, making the program much more flexible and broader.
+Ofcourse C++ and C#, etc, already follows this type of paradigm, but C is the oldest and 
+the mother language, most well recognised.
+***/
