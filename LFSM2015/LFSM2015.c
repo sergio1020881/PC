58e53cda487142460524e1c0262c2f544d1ad299
sergio <sergio1020881@users.noreply.github.com>
2015-07-09 10:46:43 +0100

    Update LFSM2015.c

--- LFSM2015/LFSM2015.c
+++ LFSM2015/LFSM2015.c
@@ -1,51 +1,107 @@
-/************************************************************************
-Title:    PCLFSM
-Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
-File:     $Id: PClfsm.h,v 0.1 2015/07/03 14:00:00 sergio Exp $
-Software: GCC
-Hardware: 
-License:  GNU General Public License 
-Usage:    
-LICENSE:
-    Copyright (C) 2015
-    This program is free software; you can redistribute it and/or modify
-    it under the consent of the code developer, in case of commercial use
-    need licence.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-COMMENT:
-	review
-************************************************************************/
-#ifndef _PCLFSM_H_
-	#define _PCLFSM_H_
 /*
-** constant and macro
+* Research
+* Nome: sergio santos
+* Contacto: 916919898; sergio.salazar.santos@gmail.com
+* data: 09-07-2015 14:00
 */
-/*
-** variable
-*/
-struct lfsm{
-	//Local Variables
-	int *mem;
-	int sizeeeprom;
-	int page;
-	int input;
-	int output;
-	//Function Pointers
-	int (*read)(struct lfsm *r, int input);
-	int (*learn)(struct lfsm *r, int input, int next);
-	int (*quant)(struct lfsm *r);
-	int (*remove)(struct lfsm *r, int input);
-	int (*deleteall)(struct lfsm *r);
-	int (*get)(struct lfsm *r);
-	int (*diff)(int xi, int xf);
-};
-typedef struct lfsm LFSM;
-/*
-** procedure and function header
-*/
-LFSM LFSMenable(int *eeprom, int sizeeeprom, int prog);
-#endif
+// fopen perror fread fwrite feof fseek ferror fclose rewind scanf sscanf getchar scanf fscanf
+#include <stdio.h>
+// calloc free realloc malloc
+#include <stdlib.h>
+// strcpy strcmp strcat memcmp
+#include <string.h>
+// termios tcflush
+//#include <termios.h>
+// nanosleep sleep
+#include <time.h>
+// tcflsuh read write close
+//#include <unistd.h>
+// perror
+#include <errno.h>
+// open
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+//#include <stdio_ext.h>
+#include <math.h>
+// personal library
+#include"PCfunction.h"
+#include"PClfsm.h"
+#include"PCficheiro.h"
+//#include"ssfiledescriptor.h"
+/***Definition and Macros***/
+#define TRUE 1
+#define EMPTY 0 //on a pc vectores are at zero therefore empty is zero.
+#define HIGH 1
+#define high 1
+#define LOW 0
+#define low 0
+/*****GLOBAL VARIABLES*****/
+int eeprom1[256];
+/*****Procedure and Function*****/
+// Main Main Main Main Main Main Main Main Main Main Main Main Main Main
+// Main Main Main Main Main Main Main Main Main Main Main Main Main Main
+int main(int argc, char *argv[])
+{
+	char *cmd;
+	int number3;
+	int number1;
+	int number2;
+	char* str=NULL;
+	//char* token[10];
+/*******************************************************/
+	FUNC func=FUNCenable();
+	LFSM r = LFSMenable(eeprom1, 256, 128);
+	/***************************/
+	FICHEIRO f=FICHEIROenable("file.txt");
+	//f.colocarchar(&f,'A');
+	//f.colocarchar(&f,'\0');
+	//f.colocarstring(&f,"qualquer coisa");
+	str=func.fltos(f.open(&f));
+	printf("%s\n\n",str);
+	//func.strtotok(str,token," ");
+	free(str);
+	f.close(&f);
+/********************************************************/
+	printf("MAYIA\n");
+	number3=func.mayia(0,1,4);
+	printf("num1: %d num2: %d magic: %d\n",1,1,number3);
+	//
+	printf("sizeeeprom: %d\n", r.sizeeeprom);
+	// Cycle
+	while(TRUE){
+		printf("write string with number\nor instruction\n");
+		cmd=func.fltos(stdin);
+		number1=func.getnum(cmd);
+		if(!strcmp(cmd,"quit") || !strcmp(cmd,"q"))
+			goto end;
+		if(!strcmp(cmd,"learn") || !strcmp(cmd,"l")){
+			printf("enter input\n");
+			cmd=func.fltos(stdin);
+			number1=func.getnum(cmd);
+			printf("enter output data\n");
+			cmd=func.fltos(stdin);
+			number2=func.getnum(cmd);
+			r.learn(&r,number1,number2);
+		}
+		if(!strcmp(cmd,"how many") || !strcmp(cmd,"h")){
+			printf("there are %d programmed.\n",r.quant(&r));
+		}
+		if(!strcmp(cmd,"delete all") || !strcmp(cmd,"d")){
+			r.deleteall(&r);
+		}
+		if(!strcmp(cmd,"remove") || !strcmp(cmd,"r")){
+			printf("enter input to remove\n");
+			cmd=func.fltos(stdin);
+			number1=func.getnum(cmd);
+			//l.remove(&l,number1);
+			r.remove(&r,number1);
+		}
+		r.read(&r,number1);
+	}
+	/******/
+	end:
+		//system("PAUSE");
+		return 0;
+}
 /***EOF***/
