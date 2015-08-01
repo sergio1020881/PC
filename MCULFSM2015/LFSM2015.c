52f78eebecbbf967d01b1de5c36aa417e6a7394a
sergio <sergio1020881@users.noreply.github.com>
2015-08-01 17:02:11 +0100

    Update LFSM2015.c

--- MCULFSM2015/LFSM2015.c
+++ MCULFSM2015/LFSM2015.c
@@ -2,8 +2,8 @@
 * Research
 * Nome: sergio santos
 * Contacto: 916919898; sergio.salazar.santos@gmail.com
-* data: 09-07-2015 14:00
-* Working on it and libraries for use in MCU, new prototype
+* data: 01-08-2015 14:00
+* working on it.
 */
 // fopen perror fread fwrite feof fseek ferror fclose rewind scanf sscanf getchar scanf fscanf
 #include <stdio.h>
@@ -38,21 +38,21 @@
 #define LOW 0
 #define low 0
 /*****GLOBAL VARIABLES*****/
-int eeprom1[256];
+unsigned int eeprom1[256];
 /*****Procedure and Function*****/
 // Main Main Main Main Main Main Main Main Main Main Main Main Main Main
 // Main Main Main Main Main Main Main Main Main Main Main Main Main Main
 int main(int argc, char *argv[])
 {
 	char *cmd;
-	int number3;
-	int number1;
-	int number2;
+	unsigned int number3;
+	unsigned int number1;
+	unsigned int number2;
 	char* str=NULL;
 	//char* token[10];
 /*******************************************************/
 	FUNC func=FUNCenable();
-	LFSM r = LFSMenable(eeprom1, 256, 120);
+	LFSM r = LFSMenable(eeprom1, 256);
 	/***************************/
 	FICHEIRO f=FICHEIROenable("file.txt");
 	//f.colocarchar(&f,'A');
@@ -69,6 +69,8 @@ int main(int argc, char *argv[])
 	printf("num1: %d num2: %d magic: %d\n",1,1,number3);
 	//
 	printf("sizeeeprom: %d\n", r.sizeeeprom);
+	printf("randomize %d\n", rand());
+	/*******************************************************************************************/
 	// Cycle
 	while(TRUE){
 		printf("write string with number\nor instruction\n");
