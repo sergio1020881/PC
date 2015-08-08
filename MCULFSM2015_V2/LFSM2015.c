87847915aa8c59452bbaf8846aa50d6dcc46dfef
sergio <sergio1020881@users.noreply.github.com>
2015-08-08 10:04:14 +0100

    Update LFSM2015.c

--- MCULFSM2015_V2/LFSM2015.c
+++ MCULFSM2015_V2/LFSM2015.c
@@ -1,9 +1,10 @@
 /*
 * Research
+* LFSM2015.c
 * Nome: sergio santos
 * Contacto: 916919898; sergio.salazar.santos@gmail.com
 * data: 07-08-2015 14:00
-* working on it.
+* 
 */
 // fopen perror fread fwrite feof fseek ferror fclose rewind scanf sscanf getchar scanf fscanf
 #include <stdio.h>
