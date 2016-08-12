f72aec21c46384a3710efa59631a4f7bb868a105
sergio <sergio1020881@users.noreply.github.com>
2016-08-12 23:04:29 +0100

    Update PCficheiro.c

--- LFSM2015/PCficheiro.c
+++ LFSM2015/PCficheiro.c
@@ -1,7 +1,7 @@
 /*************************************************************************
 Title:    PCFICHEIRO
 Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
-File:     $Id: PCficheiro.c, v 0.1 2015/02/06 14:00:00 sergio Exp $
+File:     $Id: PCficheiro.c, v 0.1 2015/08/12 14:00:00 sergio Exp $
 Software: GCC
 Hardware:  
 License:  GNU General Public License        
@@ -52,6 +52,7 @@ COMMENT:
 /*
 ** variable
 */
+int errno;
 /*
 ** procedure and function header
 */	
@@ -61,26 +62,40 @@ COMMENT:
 FICHEIRO FICHEIROenable(char *filename)
 {
 	/***local variables***/
+	errno=0;
 	/***Declare Functions***/
 	int FICHEIROclose(struct ficheiro *f);
 	int FICHEIROputc(struct ficheiro *f, int c);
 	int FICHEIROputs(struct ficheiro *f, const char* s);
 	FILE* FICHEIROopen(struct ficheiro *f);
+	void FICHEIROrewind(struct ficheiro *f);
 	/******/
 	FICHEIRO f;
 	//Inicialize varibles
 	strcpy(f.filename,filename);
+	//do a checkup if exists first !
 	strcpy(f.permision,"a+");//setting as default
 	//Functions pointers or Vtable to declared functions
 	f.open=FICHEIROopen;
 	f.colocarchar=FICHEIROputc;
 	f.colocarstring=FICHEIROputs;
 	f.close=FICHEIROclose;
+	f.rewind=FICHEIROrewind;
 	//procedures
 	f.fp=fopen(f.filename,f.permision);
-	if(f.fp==NULL){
-		perror("ficheiro at fopen");
+	if(f.fp!=NULL){
+		printf("Opening file %s\n",f.filename);
+	}else{
+		perror("Ficheiro at fopen\n");
+		printf("Creating file %s\n",f.filename);
+		f.fp=fopen(f.filename,f.permision);
 	}
+	f.fd=fileno(f.fp);
+	if(f.fd<0){
+		perror("Ficheiro filedescriptor not existent\n");
+		//sprintf(f.errcode,"None errno: %d\n", errno);
+	}
+	//fgetpos(f.fp,&f.pos);
 	//return result
 	return f;
 }
@@ -111,6 +126,10 @@ int FICHEIROputs(struct ficheiro *f, const char* s)
 FILE* FICHEIROopen(struct ficheiro *f)
 {
 	return f->fp;
+}
+void FICHEIROrewind(struct ficheiro *f)
+{
+	//fsetpos(f->fp,0);
 }
 /*
 ** interrupt
