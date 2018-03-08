4ab46e81189ce6ed57478882cdd3d2727812ef21
sergio <sergio1020881@users.noreply.github.com>
2018-03-08 10:44:04 +0000

    Update PClfsm.h

--- MCULFSM2018_v2/PClfsm.h
+++ MCULFSM2018_v2/PClfsm.h
@@ -3,9 +3,9 @@ Title:    PCLFSM
 Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
 File:     $Id: PClfsm.h,v 0.1 2015/08/12 14:00:00 sergio Exp $
 Software: GCC
-Hardware: 
-License:  GNU General Public License 
-Usage:    
+Hardware:
+License:  GNU General Public License
+Usage:
 LICENSE:
     Copyright (C) 2015
     This program is free software; you can redistribute it and/or modify
@@ -68,11 +68,11 @@ LFSM LFSMenable(LFSMDATA *eeprom, unsigned int sizeeeprom);
 Sintax Semantic Pragmatic.
 Sintax is a key principle able to follow a sintactical work flow that solves any problem,
 having a concise structural work flow is very importante.
-The method used is C object oriented aproach, do not know anybody using this type of aproach, 
-but all the programs made here follow this type of paradigm, it is very very stable either in 
+The method used is C object oriented aproach, do not know anybody using this type of aproach,
+but all the programs made here follow this type of paradigm, it is very very stable either in
 PC or Embeded MCU programs, making Main program very compact.
-Discovered this method by experience since then never left it, altering prior programs to fit 
+Discovered this method by experience since then never left it, altering prior programs to fit
 its syntax, making the program much more flexible and broader.
-Ofcourse C++ and C#, etc, already follows this type of paradigm, but C is the oldest and 
+Ofcourse C++ and C#, etc, already follows this type of paradigm, but C is the oldest and
 the mother language, most well recognised.
 ***/
