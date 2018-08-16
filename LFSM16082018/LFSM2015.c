58608ecdeb7ad3683bf5c2c9b67b9fd78203fd0f
sergio <sergio1020881@users.noreply.github.com>
2018-08-16 09:26:45 +0100

    Add files via upload

---
+++ LFSM16082018/LFSM2015.c
@@ -0,0 +1,168 @@
+/*
+* Research
+* PCLFSM2015.c
+* Nome: sergio santos
+* Contacto: 916919898; sergio.salazar.santos@gmail.com
+* data: 24-02-2018 11:00
+*
+*/
+// fopen perror fread fwrite feof fseek ferror fclose rewind scanf sscanf getchar scanf fscanf
+#include <stdio.h>
+// calloc free realloc malloc
+#include <stdlib.h>
+// strcpy strcmp strcat memcmp
+#include <string.h>
+// termios tcflush
+//#include <termios.h>
+// nanosleep sleep
+#include <time.h>
+// tcflsuh read write close
+//#include <unistd.h>
+// perror
+#include <errno.h>
+// open
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+//#include <stdio_ext.h>
+#include <math.h>
+// personal library
+#include"PCfunction.h"
+#include"PClfsm.h"
+#include"PCficheiro.h"
+//#include"ssfiledescriptor.h"
+/***Definition and Macros***/
+#define TRUE 1
+#define EMPTY 0 //on a pc vectores are at zero therefore empty is zero.
+#define HIGH 1
+#define high 1
+#define LOW 0
+#define low 0
+//#ifdef linux
+//#elif _WIN32
+//#else
+//#endif
+/*****GLOBAL VARIABLES*****/
+LFSMDATA MEM[128];
+/*={
+{},
+{}
+};
+*/
+/*****Procedure and Function*****/
+void* this(void* address){
+	return address;
+}
+// Main Main Main Main Main Main Main Main Main Main Main Main Main Main
+// Main Main Main Main Main Main Main Main Main Main Main Main Main Main
+int main(int argc, char *argv[])
+{
+	/*
+	*** Capture arguments
+	*/
+	printf("Running program - %s with - %d arguments", argv[0], argc);
+	/*
+	*** Local variables
+	*/
+	char *cmd;
+	unsigned int number3;
+	unsigned int number1;
+	unsigned int number2;
+	char* str=NULL;
+	//char* token[10];
+/*******************************************************/
+	FUNC func=FUNCenable();
+	LFSM r = LFSMenable(MEM, 128);
+	/***************************/
+	FICHEIRO f=FICHEIROenable("file.txt");
+	//f.rewind(this(&f));
+	f.colocarchar(this(&f),'A');
+	f.colocarstring(this(&f),"qualquer coisa");
+	str=func.fltos(f.open(this(&f)));
+	printf("%s\n\n",str);
+	//func.strtotok(str,token," ");
+	free(str);
+	f.close(this(&f));
+	printf("putstringtest: %s\n", func.putstr("hello world"));
+	printf("lfsmdata size: %d\n",sizeof(LFSMDATA));
+/********************************************************/
+	printf("MAYIA\n");
+	number3=func.mayia(0,1,4);
+	printf("num1: %d num2: %d magic: %d\n",1,1,number3);
+	//
+	printf("sizeeeprom: %d\n", r.sizeeeprom);
+	printf("randomize %d\n", rand());
+	/*******************************************************************************************/
+	// Cycle
+	while(TRUE){
+		printf("write string with number\nor instruction\n");
+		cmd=func.fltos(stdin);
+		number1=func.getnum(cmd);
+		if(!strcmp(cmd,"quit") || !strcmp(cmd,"q")){
+			free(cmd);
+			goto end;
+		}
+		if(!strcmp(cmd,"learn") || !strcmp(cmd,"l")){
+			printf("enter input\n");
+			free(cmd);
+			cmd=func.fltos(stdin);
+			//number1=func.binary_decimal(func.getnum(cmd));
+			number1=func.getnum(cmd);
+			printf("enter output data\n");
+			free(cmd);
+			cmd=func.fltos(stdin);
+			number2=func.getnum(cmd);
+			printf("enter page\n");
+			free(cmd);
+			cmd=func.fltos(stdin);
+			number3=func.getnum(cmd);
+			printf("Entered values %d %d %d\n",number1,number2,number3);
+			r.learn(this(&r),number1,number2,number3);
+		}
+		if(!strcmp(cmd,"how many") || !strcmp(cmd,"n")){
+			printf("there are %d programmed.\n",r.quant(this(&r)));
+		}
+		if(!strcmp(cmd,"delete all") || !strcmp(cmd,"d")){
+			r.deleteall(this(&r));
+		}
+		if(!strcmp(cmd,"remove") || !strcmp(cmd,"r")){
+			printf("enter input to remove\n");
+			free(cmd);
+			cmd=func.fltos(stdin);
+			number1=func.getnum(cmd);
+			//l.remove(&l,number1);
+			r.remove(this(&r),number1);
+		}
+		if(!strcmp(cmd,"help") || !strcmp(cmd,"h")){
+			printf("Possible commands:\n");
+           		printf("\tquit - q\n");
+            		printf("\tlearn - l\n");
+            		printf("\thelp - h\n");
+            		printf("\thow many - n\n");
+            		printf("\tdelete all - d\n");
+            		printf("\tremove\n");
+            		printf("\toptions\n");
+            		printf("Page setting:\n");
+            		printf("\t1 is for global logic\n");
+            		printf("\t2 is for local logic\n");
+            		printf("\t3 is for sequencial logic\n");
+            		printf("Input procedure:\n");
+            		printf("\tProgram sequence is first input desired\n");
+			printf("\tsecond is desired ouput\n");
+			printf("\tthen select what page to store in refered as above.\n");
+			free(cmd);
+			continue;
+		}
+		if(!strcmp(cmd,"options") || !strcmp(cmd,"o")){
+			printf("learn or l\nquit or q\nhow many or n\ndelete all or d\nremove or r\noptions\n");
+			free(cmd);
+		}
+		printf("->->->->->->->->->->->->->->-> %s \n",func.print_binary(r.read(this(&r),number1)));
+	free(cmd);
+	}
+	/******/
+	end:
+		//system("PAUSE");
+		return 0;
+}
+/***EOF***/
---
+++ LFSM16082018/PClfsm.c
@@ -0,0 +1,395 @@
+/*************************************************************************
+Title:    PCLFSM
+Author:   Sergio Manuel Santos <sergio.salazar.santos@gmail.com>
+File:     $Id: PClfsm.c, v 0.1 2018/02/24 11:00:00 sergio Exp $
+Software: GCC
+Hardware:
+License:  GNU General Public License
+DESCRIPTION:
+	PC emulation
+USAGE:
+NOTES:
+LICENSE:
+    Copyright (C) 2015
+    This program is free software; you can redistribute it and/or modify
+    it under the consent of the code developer, in case of commercial use
+    need licence.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+COMMENT:
+	working pretty good, trial more.
+	page=1 is dedicated for global logic, page>=2 local logic, if page>2.
+*************************************************************************/
+/*
+** library
+*/
+// fopen perror fread fwrite feof fseek ferror fclose rewind scanf sscanf getchar scanf fscanf
+#include <stdio.h>
+// calloc free realloc malloc
+//#include <stdlib.h>
+// strcpy strcmp strcat memcmp
+//#include <string.h>
+// termios tcflush
+//#include <termios.h>
+// nanosleep sleep
+//#include <time.h>
+// tcflsuh read write close
+//#include <unistd.h>
+// perror
+//#include <errno.h>
+// open
+//#include <sys/types.h>
+//#include <sys/stat.h>
+//#include <fcntl.h>
+//#include <stdio_ext.h>
+//#include <math.h>
+/***/
+#include"PClfsm.h"
+/*
+** constant and macro
+*/
+#define EMPTY 0
+/*
+** variable
+*/
+LFSMDATA data;
+/*
+unsigned int mem[]=
+{pin state, feedback, pin mask, output}
+*/
+/*
+** procedure and function header
+*/
+unsigned int LFSMread(struct lfsm *r, unsigned int input);
+unsigned int LFSMlearn(struct lfsm *r, unsigned int input, unsigned int next, unsigned int page);
+unsigned int LFSMquant(struct lfsm *r);
+unsigned int LFSMremove(struct lfsm *r, unsigned int input);
+unsigned int LFSMdeleteall(struct lfsm *r);
+/*
+** Object Inicialize
+*/
+LFSM LFSMenable(LFSMDATA *eeprom, unsigned int sizeeeprom)
+{
+	/***Local Variable***/
+	printf("\tLFSMenable\n");
+	/***Local Function Header***/
+	unsigned int LFSMgetoutput(struct lfsm *r);
+	void LFSMsetoutput(struct lfsm *r, unsigned int output);
+	unsigned int LFSMlh(unsigned int xi, unsigned int xf);
+	unsigned int LFSMhl(unsigned int xi, unsigned int xf);
+	unsigned int LFSMoutputcalc(unsigned int feedback, unsigned int hl, unsigned int lh);
+	unsigned int LFSMdiff(unsigned int xi, unsigned int xf);
+	/***Create Object***/
+	LFSM r;
+	//Inicialize variables
+	r.mem=eeprom;
+	r.sizeeeprom=sizeeeprom;
+	r.page=0;//page
+	r.input=0;//input
+	r.output=0;//output
+	//Function Vtable
+	r.read=LFSMread;
+	r.learn=LFSMlearn;
+	r.quant=LFSMquant;
+	r.remove=LFSMremove;
+	r.deleteall=LFSMdeleteall;
+	r.get=LFSMgetoutput;
+	r.set=LFSMsetoutput;
+	r.lh=LFSMlh;
+	r.hl=LFSMhl;
+	r.outputcalc=LFSMoutputcalc;
+	r.diff=LFSMdiff;
+	/******/
+	return r;
+}
+/*
+** procedure and function
+*/
+/***read***/
+unsigned int LFSMread(struct lfsm *r, unsigned int input)
+{
+	unsigned int i1;
+	unsigned int status=0; //empty
+	unsigned int HL,LH;
+	printf("-\tLFSMread\n");
+	HL=r->hl(r->input,input);
+	LH=r->lh(r->input,input);
+	if(HL || LH){
+		for(i1=0;i1<r->sizeeeprom;i1++){
+			data=r->mem[i1];//upload eeprom data
+			if(data.page){
+				/******/
+				switch(data.page){
+					case 1: //read
+						if( data.inhl==HL && data.inlh==LH ){
+						    //Global logic
+                            i1=r->sizeeeprom;
+                            status=1;
+                        }
+						break;
+					default:
+						if( data.feedback==r->output && data.inhl==HL && data.inlh==LH ){
+						    //Local logic
+                            i1=r->sizeeeprom;
+                            status=2;
+                        }
+						break;
+				};
+			}else
+                status=3;
+		}
+	}
+	switch (status){
+		case 0:
+			printf("LFSMread: No entry\n");
+			r->input=input;//detailed capture
+			break;
+		case 1:
+            printf("LFSMread: Global logic\n");
+			r->page=data.page;
+			r->input=input;//detailed capture
+			r->output=r->outputcalc(data.feedback,data.outhl,data.outlh);
+			break;
+		case 2:
+			printf("LFSMread: Local logic\n");
+			r->page=data.page;
+			r->input=input;//detailed capture
+			r->output=r->outputcalc(data.feedback,data.outhl,data.outlh);
+			break;
+		case 3:
+			printf("LFSMread: Entry Not recognized\n");
+			r->input=input;//detailed capture
+			break;
+		default:
+			//impossible situation
+			break;
+	}
+	printf("input: %d\n",r->input);
+	return r->output;
+}
+/***learn***/
+unsigned int LFSMlearn(struct lfsm *r, unsigned int input, unsigned int next, unsigned int page)
+{
+	unsigned int i1;
+	unsigned int status=0;
+	unsigned int HL,LH;
+	printf("-\tLFSMlearn\n");
+	HL=r->hl(r->input,input);
+	LH=r->lh(r->input,input);
+	if(page>0){
+		if(HL || LH){
+			for(i1=0;i1<r->sizeeeprom;i1++){
+				data=r->mem[i1];//upload eeprom data
+				if(data.page){
+					/******/
+					if((data.page==1 && data.inhl==HL && data.inlh==LH) || (data.feedback==r->output && data.inhl==HL && data.inlh==LH)){
+						status=1;//not permitted
+						break;
+					}
+				}
+			status=2;//not existente
+			}
+		}
+	}
+	switch (status){
+		case 0:
+			printf("LFSMlearn: No Operation.\n");
+			break;
+		case 1:
+			printf("LFSMlearn: not permitted.\n");
+			break;
+		case 2:
+			printf("LFSMlearn: going to try add new program.\n");
+			//prepare data to write to eeprom
+			data.page=page;
+			data.feedback=r->output;
+			data.input=r->input;
+			data.inhl=HL;
+			data.inlh=LH;
+			data.outhl=r->hl(r->output,next);
+			data.outlh=r->lh(r->output,next);
+			printf("%d  %d  %d  %d  %d  %d  %d\n",data.page,data.feedback,data.input,data.inhl,data.inlh,data.outhl,data.outlh);
+			for(i1=0;i1<r->sizeeeprom;i1++){
+				//search empty space em memory
+				if(r->mem[i1].page==EMPTY){
+					//write data to eeprom
+					r->mem[i1]=data;
+					status=3;//created
+					break;
+				}
+				status=4;//not possible
+			}
+		case 3:
+			printf("LFSMlearn: succesfully added.\n");
+			break;
+		case 4:
+			printf("LFSMlearn: memmory full.\n");
+			break;
+		default:
+			break;
+	}
+	return status;
+}
+/***quant***/
+unsigned int LFSMquant(struct lfsm *r)
+{
+	unsigned int i1;
+	unsigned int programmed;
+	printf("-\tLFSMquant\n");
+	for(i1=0,programmed=0;i1<r->sizeeeprom;i1++){
+		data=r->mem[i1];//upload data from eeprom
+		if(data.page!=EMPTY){
+			printf("page:%d feedback:%d  input:%d  inhl:%d  inlh:%d  outhl:%d  outlh:%d\n",data.page,data.feedback,data.input,data.inhl,data.inlh,data.outhl,data.outlh);
+			programmed++;
+		}
+	}
+	return programmed;
+}
+/***remove***/
+unsigned int LFSMremove(struct lfsm *r, unsigned int input)
+{
+	unsigned int i1;
+	unsigned int keyfound;
+	unsigned int status=0;
+	unsigned int HL,LH;
+	printf("-\tLFSMremove\n");
+	HL=r->hl(r->input,input);
+	LH=r->lh(r->input,input);
+	for(i1=0;i1<r->sizeeeprom;i1++){
+		data=r->mem[i1];//upload data from eeprom
+		if(data.page){
+			/******/
+			switch(data.page){
+				case 1:
+					keyfound=(
+						data.inhl==HL &&
+						data.inlh==LH
+						);//bool
+					break;
+				case 2:
+					keyfound=(
+						data.feedback==r->output &&
+						data.inhl==HL &&
+						data.inlh==LH
+						);//bool
+					break;
+				default:
+					keyfound=(
+						data.feedback==r->output &&
+						data.input==r->input &&
+						data.inhl==HL &&
+						data.inlh==LH
+						);//bool
+					break;
+			};
+			/******/
+			if(keyfound){
+				status=1;//remove
+				break;
+			}
+		}
+		status=2;//does not exist
+	}
+	switch (status){
+		case 0:
+			printf("LFSMremove: No operation\n");
+			break;
+		case 1:
+			printf("LFSMremove: Removed: %d\n",status);
+			//descativate memory space, write to eeprom empty space.
+			r->mem[i1].page=EMPTY;
+			break;
+		case 2:
+			printf("LFSMremove: Not existent: %d\n",status);
+			break;
+		default:
+			break;
+	}
+	return status;
+}
+/***deleteall***/
+unsigned int LFSMdeleteall(struct lfsm *r)
+{
+	unsigned int i1;
+	unsigned int status=0;
+	printf("-\tLFSMdeleteall\n");
+	if(!status){//not removed
+		for(i1=0;i1<r->sizeeeprom;i1++){
+			//read eeprom check if memory space has data
+			if(r->mem[i1].page){
+				//desactivate memory space to empty
+				r->mem[i1].page=EMPTY;
+				status=1;//all deleted
+			}
+		}
+	}
+	r->output=0;
+	printf("Done\n");
+	return status;
+}
+/***get***/
+unsigned int LFSMgetoutput(struct lfsm *r)
+{
+	printf("-\tLFSMgetoutput\n");
+	return r->output;
+}
+/***set***/
+void LFSMsetoutput(struct lfsm *r, unsigned int output)
+{
+	printf("-\tLFSMsetoutput\n");
+	r->output=output;
+}
+/***lh***/
+unsigned int LFSMlh(unsigned int xi, unsigned int xf)
+{
+	unsigned int i;
+	printf("-\tLFSMlh\n");
+	i=xf^xi;
+	i&=xf;
+	return i;
+}
+/***hl***/
+unsigned int LFSMhl(unsigned int xi, unsigned int xf)
+{
+	unsigned int i;
+	printf("-\tLFSMhl\n");
+	i=xf^xi;
+	i&=xi;
+	return i;
+}
+/***output***/
+unsigned int LFSMoutputcalc(unsigned int feedback, unsigned int hl, unsigned int lh)
+{
+	printf("-\tLFSMoutputcalc\n");
+	feedback|=lh;
+	feedback&=~hl;
+	return feedback;
+}
+/***diff***/
+unsigned int LFSMdiff(unsigned int xi, unsigned int xf)
+{
+	printf("-\tLFSMdiff\n");
+	return xi^xf;
+}
+/*
+** interrupt
+*/
+/***EOF***/
+/***Comments**
+Reality works like this:
+keyfound=(
+	block[LFSM_feedback]==r->output &&
+	block[LFSM_mask]==mask &&
+	block[LFSM_maskedinput]==(mask&input)
+);//bool, block[1] is masked bits, block[1] is bits state
+**************
+NOTES:
+unsigned int vect[]=
+{
+mask,mask&pinstate,feedback,output,
+};
+**************
+try to make more depth choice of database research or option selections.
+*************/
